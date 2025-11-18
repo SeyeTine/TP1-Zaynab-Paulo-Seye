d1353b06d75dd6e986aaca67b87c5903895e5132
SeyeTine <tine.seye@gmail.com>
2025-11-18 18:16:01 -0500

    Test adherent

--- Classe Emprunt/Classe  Emprunt.py
+++ Classe Emprunt/Classe  Emprunt.py
@@ -8,4 +8,4 @@ class Emprunt:
     def __str__(self):
         retour = self.date_retour if self.date_retour else "Non rendu"
         return str(self.adherent) + " -> '" + self.livre.titre + "' (Emprunt : " + self.date_emprunt + ", Retour : " + retour + ")"
-
+p
