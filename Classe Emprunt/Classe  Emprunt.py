c075af8644c20a16e676f1095088ccb1ff346248
SeyeTine <tine.seye@gmail.com>
2025-11-18 17:34:01 -0500

    classe emprunt

---
+++ Classe Emprunt/Classe  Emprunt.py
@@ -0,0 +1,10 @@
+class Emprunt:
+    def __init__(self, adherent, livre, date_emprunt=None, date_retour=None):
+        self.adherent = adherent
+        self.livre = livre
+        self.date_emprunt = date_emprunt or datetime.now().strftime("%Y-%m-%d")
+        self.date_retour = date_retour
+
+    def __str__(self):
+        retour = self.date_retour if self.date_retour else "Non rendu"
+        return str(self.adherent) + " -> '" + self.livre.titre + "' (Emprunt : " + self.date_emprunt + ", Retour : " + retour + ")"
